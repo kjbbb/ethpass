43b469e0f0ece47dddde1a8ce1ee2f147f6e2c36
kevin berry <kevinjb465@gmail.com>
2017-11-29 00:12:17 -0800

    add list selection logic to sidebar and center pane

--- src/app.js
+++ src/app.js
@@ -26,9 +26,14 @@ class Store {
 
 const SidebarView = observer(({store}) => {
 
+    let fnSelect = (idx) => {
+        store.selected = idx;
+        console.log(idx);
+    }
+
     let pwA = store.passwordA.map((pw, idx) => {
         return (
-            <tr key={idx} ><th scope="row"></th>
+            <tr key={idx} onClick={fnSelect.bind(this, idx)}><th scope="row"></th>
               <td>
                 <p className="nomargin"><strong>{pw.name}</strong></p>
                 <p className="text-muted nomargin">{pw.username}</p>
@@ -53,6 +58,11 @@ const SidebarView = observer(({store}) => {
 const CenterpaneView = observer(({store}) => {
 
     let pw = store.passwordA[store.selected];
+
+    //we should use id here instead
+    let fnDelete = (idx) => {
+        store.passwordA.splice(idx, 1);
+    }
 
     //todo, if store is empty, display zero state
 
@@ -81,7 +91,7 @@ const CenterpaneView = observer(({store}) => {
         <br />
       <div className="text-center">
         <button className="btn btn-default">update </button>
-        <button className="btn btn-danger"> delete</button>
+        <button className="btn btn-danger" onClick={fnDelete.bind(this, store.selected)}> delete</button>
       </div>
     </div>
     )
@@ -92,23 +102,35 @@ window.onload = () => {
     //let Contract = web3.eth.contract(contract.abi);
     //ContractInst = Contract.at(contract.address);
 
-    let pw = {
+    let store = new Store();
+    store.selected = 0;
+
+    store.passwordA.push({
         name: 'lol',
         username: 'memes',
         password: 'nyancat',
         ctime: 'nov 1 2017',
         mtime: 'nov 1 2017',
         notes: 'some notes here'
-    };
+    });
 
-    let store = new Store();
-    store.selected = 0;
+    store.passwordA.push({
+        name: 'second',
+        username: 'memers',
+        password: 'suprb0',
+        ctime: 'nov 2 2017',
+        mtime: 'nov 2 2017',
+        notes: 'ayy more notes'
+    });
 
-    store.passwordA.push(pw);
-    store.passwordA.push(pw);
-    store.passwordA.push(pw);
-    store.passwordA.push(pw);
-    store.passwordA.push(pw);
+    store.passwordA.push({
+        name: 'feels',
+        username: 'good',
+        password: 'man',
+        ctime: 'nov 3 2017',
+        mtime: 'nov 3 2017',
+        notes: 'so many memes'
+    });
 
     ReactDOM.render(<SidebarView store={store} />, document.getElementById("sidebar"));
     ReactDOM.render(<CenterpaneView store={store} />, document.getElementById("centerpane"));
